f34b729f6029039bd83b9c735b2af2c7564ad4a0
ddaeunbb <ddaeunbb@gmail.com>
2023-05-31 16:08:41 +0900

    [create]:productList-page

--- project/src/components/filter/Filter.tsx
+++ project/src/components/filter/Filter.tsx
@@ -1,35 +1,49 @@
 import tw from "tailwind-styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../modules";
+import { setTab } from "../../modules/filterSlice";
+import { CategoryName } from "../../types/categories";
+import Categories from "../../types/categories";
+
+// png
 import total from '../../assets/filter/total.png'
 import smartphone from '../../assets/filter/smartphone.png'
 import laptop from '../../assets/filter/laptop.png'
 import automotive from '../../assets/filter/automotive.png'
 import motocycle from '../../assets/filter/motorcycle.png'
 
+
 export default function Filter() {
+  const dispatch = useDispatch();
+  const category = useSelector((state: RootState) => state.filterList.category);
+  const tabHandler = (category: CategoryName)=>{
+    dispatch(setTab(category));
+  }
+
   return (
     <FilterContainer>
       <FilterTab>
-        <FilterImg src={total}/>
+        <FilterImg isChecked={category === Categories.total} src={total} onClick={()=>tabHandler(Categories.total)}/>
         <FilterText>전체</FilterText>
       </FilterTab>
 
       <FilterTab>
-        <FilterImg src={smartphone}/>
+        <FilterImg isChecked={category === Categories.smartphones} src={smartphone} onClick={()=>tabHandler(Categories.smartphones)}/>
         <FilterText>스마트폰</FilterText>
       </FilterTab>
 
       <FilterTab>
-        <FilterImg src={laptop}/>
+        <FilterImg isChecked={category === Categories.laptops} src={laptop} onClick={()=>tabHandler(Categories.laptops)}/>
         <FilterText>노트북</FilterText>
       </FilterTab>
 
       <FilterTab>
-      <FilterImg src={automotive}/>
+      <FilterImg isChecked={category === Categories.automotive} src={automotive} onClick={()=>tabHandler(Categories.automotive)}/>
       <FilterText>자동차</FilterText>
       </FilterTab>
 
       <FilterTab>
-      <FilterImg src={motocycle}/>
+      <FilterImg isChecked={category === Categories.motorcycle} src={motocycle} onClick={()=>tabHandler(Categories.motorcycle)}/>
       <FilterText>오토바이</FilterText>
       </FilterTab>
 
@@ -43,7 +57,7 @@ const FilterContainer = tw.ul`
   flex
   h-43;
   gap-5
-  my-5
+  my-10
   justify-center
 `
 
@@ -52,12 +66,17 @@ const FilterTab = tw.li`
   text-center
 `
 
-const FilterImg = tw.img`
+interface FilterImgProps {
+  isChecked : boolean;
+}
+
+const FilterImg = tw.img<FilterImgProps>`
   w-16
   h-16
   border
   rounded-full
   p-2
+  ${(props) => props.isChecked && 'bg-indigo-300'}
   hover:bg-indigo-300
   cursor-pointer
 `
--- project/src/pages/products/Products.tsx
+++ project/src/pages/products/Products.tsx
@@ -2,9 +2,44 @@ import tw from "tailwind-styled-components";
 import Filter from "../../components/filter/Filter";
 import { useSelector } from "react-redux";
 import { RootState } from "../../modules";
+import Categories from "../../types/categories";
 import ApiDataInterFace from "../../modules/apidata.interface";
 import ProductCard from "../../components/productCard/ProductCard";
 
+export default function Products() {
+  const productList = useSelector((state: RootState)  => state.productList.products);
+  const category = useSelector((state: RootState) => state.filterList.category);
+
+  switch(category){
+    case Categories.total :
+      return (
+        <ProductsContainer>
+          <Filter />
+          <ProductCardContainer>
+            {productList.map((product: ApiDataInterFace) => (
+            <ProductCard key={product.id} product={product} />
+            ))}
+          </ProductCardContainer>
+        </ProductsContainer>
+      )
+    
+    default :
+      return (
+        <ProductsContainer>
+          <Filter />
+          <ProductCardContainer>
+            {productList // 
+              .filter((product: ApiDataInterFace) => product.category === category)
+              .map((product: ApiDataInterFace) =>
+              (<ProductCard key={product.id} product={product} />)
+            )}
+          </ProductCardContainer>
+        </ProductsContainer>
+      )
+  }
+}
+
+// tailwind
 const ProductsContainer = tw.div`
   w-100vw;
   flex-col
@@ -17,21 +52,6 @@ const ProductCardContainer = tw.div`
   flex-wrap
   justify-center
   gap-5
-  py-10
+  py-5
   mx-14
 `;
-
-export default function Products() {
-  const productList = useSelector((state: RootState)  => state.productList.products);
-
-  return (
-    <ProductsContainer>
-      <Filter />
-      <ProductCardContainer>
-        {productList.map((product: ApiDataInterFace) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </ProductCardContainer>
-    </ProductsContainer>
-  )
-}
