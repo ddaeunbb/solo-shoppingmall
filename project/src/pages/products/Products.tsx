6fd0808ee589e53ae2b7becbc23d9e45c2480abf
ddaeunbb <ddaeunbb@gmail.com>
2023-06-01 21:02:11 +0900

    [setting]:unlimit-scroll

--- project/src/pages/products/Products.tsx
+++ project/src/pages/products/Products.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import tw from "tailwind-styled-components";
 import Filter from "../../components/filter/Filter";
+import { useRef, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../modules";
 import { closeModal } from "../../modules/hamburgerModalSlice";
@@ -8,9 +9,25 @@ import Categories from "../../types/categories";
 import ProductCard from "../../components/productCard/ProductCard";
 
 const Products : FC  = () => {
+  const scrollRef = useRef<number | null>(null);
   const dispatch = useDispatch();
   const productList = useSelector((state: RootState)  => state.productList.products);
   const category = useSelector((state: RootState) => state.filterList.category);
+
+  const defaultPage: number = 8;
+  const [page, setPage] = useState(defaultPage);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      scrollRef.current = window.scrollY;
+      const scrollPosition = window.innerHeight + document.documentElement.scrollTop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      if (scrollPosition === scrollHeight) setPage(page + defaultPage);
+    };
+
+    window.addEventListener("scroll", scrollListener);
+    return () => window.removeEventListener("scroll", scrollListener);
+  }, [page, setPage]);
 
   switch(category){
     case Categories.total :
@@ -18,7 +35,7 @@ const Products : FC  = () => {
         <ProductsContainer onClick={()=> dispatch(closeModal())}>
           <Filter />
           <ProductCardContainer>
-            {productList.map(product => 
+            {productList.slice(0, page).map(product => 
               (<ProductCard key={product.id} product={product} />)
             )}
           </ProductCardContainer>
