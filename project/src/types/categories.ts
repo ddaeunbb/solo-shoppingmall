ecfad145d8612d0578ef9061722547ee594ff841
ddaeunbb <ddaeunbb@gmail.com>
2023-06-08 16:41:14 +0900

    refactoring/enum

--- project/src/types/categories.ts
+++ project/src/types/categories.ts
@@ -1,10 +1,10 @@
-enum Categories {
-  total = 'total',
-  smartphones = 'smartphones',
-  laptops = 'laptops',
-  automotive = 'automotive',
-  motorcycle = 'motorcycle',
-}
+const Categories = {
+  total: 'total',
+  smartphones: 'smartphones',
+  laptops: 'laptops',
+  automotive: 'automotive',
+  motorcycle: 'motorcycle',
+} as const
 
 export default Categories
 export type CategoryName = 'total' | 'smartphones' | 'laptops' | 'automotive' | 'motorcycle'
